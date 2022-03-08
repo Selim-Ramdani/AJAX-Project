aa016200d232332a444c435aaf692570a99c8552
curlyroots <selimcodeur@gmail.com>
2022-03-08 22:51:44 +0100

    Ajout de la page mailing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+
   resources :books, only: [:create, :index]
   resources :todos, only: [:index, :create]
+  resources :emails
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_134559) do
+ActiveRecord::Schema.define(version: 2022_03_08_210110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_03_08_134559) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "object"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
